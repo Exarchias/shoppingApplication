be068a1a77923e4e6291f41f2f7a3b29573e9498
Robert Kristian Alm <exarchiasghost@gmail.com>
2020-02-17 23:37:45 +0100

    Populating the Database with a CallBack.
    For now it used for testing purposes but later it will be definitely used for  a hardcoded superuser, (for safety reasons)

--- shoppingapp/app/src/main/java/com/example/shoppingapplication/NoteDatabase.java
+++ shoppingapp/app/src/main/java/com/example/shoppingapplication/NoteDatabase.java
@@ -1,16 +1,19 @@
 package com.example.shoppingapplication;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 //Robert: This is the actual DataBase
 @Database(entities = {Note.class, User.class, Item.class}, version = 1)
 public abstract class NoteDatabase extends RoomDatabase {
 
-    //Robert: it is a singlenton. That means that no second Database can be initiated.
+    //Robert: it is a singleton pattern. That means that no second Database can be initiated.
     private static NoteDatabase instance;
 
     public abstract NoteDao noteDao();
@@ -20,8 +23,37 @@ public abstract class NoteDatabase extends RoomDatabase {
             instance = Room.databaseBuilder(context.getApplicationContext(),
                     NoteDatabase.class, "note_database")
                     .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)
                     .build();
         }
         return instance;
+    }
+
+    //Robert: This is used for the hardcoded population of the Data Base.
+    //Now it is used for testing purposes, BUT it will be surely be a Superuser,
+    //for safety reasons
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbAsyncTask(instance).execute();
+        }
+    };
+
+    //ASYNC for popylating the data base on creation. Try to avoid changes here.
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
+        private NoteDao noteDao;
+
+        private PopulateDbAsyncTask(NoteDatabase db) {
+            noteDao = db.noteDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.insert(new Note("Title 1", "Description 1", 1));
+            noteDao.insert(new Note("Title 2", "Description 2", 2));
+            noteDao.insert(new Note("Title 3", "Description 3", 3));
+            return null;
+        }
     }
 }
