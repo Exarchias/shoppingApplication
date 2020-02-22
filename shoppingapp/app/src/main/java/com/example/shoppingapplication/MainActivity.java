e7f212ae1566e27836402cb0b566f30fb277d02b
Robert Kristian Alm <exarchiasghost@gmail.com>
2020-02-22 15:03:24 +0100

    Extremelly important test.
    This test will lead to a very efficient and easy to use functionality that I am planning to implement today.

--- shoppingapp/app/src/main/java/com/example/shoppingapplication/MainActivity.java
+++ shoppingapp/app/src/main/java/com/example/shoppingapplication/MainActivity.java
@@ -5,10 +5,12 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import java.util.List;
+import java.util.ListIterator;
 
 public class MainActivity extends AppCompatActivity {
     private NoteViewModel noteViewModel;
@@ -25,6 +27,27 @@ public class MainActivity extends AppCompatActivity {
                 //Tests the handshake by detecting the if the entities are updated.
                 //I wish to implement better testing scripts bur I did enough for now.
                 Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+                //this is how we take the objects from the list. But we need some kind of itteration.
+                Note note = noteViewModel.getAllNotes().getValue().get(0);
+                String msg = note.getTitle();
+                String msg2 = "";
+                //Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                //notes.listIterator();
+
+                // Getting ListIterator
+                ListIterator<Note> notesIterator = notes.listIterator();
+
+                // Traversing elements
+                int count = 0;
+                while(notesIterator.hasNext()){
+                    //msg = String.valueOf(count);
+                    count++;
+                    //Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    msg2 = msg2 + notesIterator.next().getTitle() + " " + count + " ";
+                    //Toast.makeText(MainActivity.this, msg2, Toast.LENGTH_SHORT).show();
+                }
+
+                Toast.makeText(MainActivity.this, msg2, Toast.LENGTH_SHORT).show();
             }
         });
     }
