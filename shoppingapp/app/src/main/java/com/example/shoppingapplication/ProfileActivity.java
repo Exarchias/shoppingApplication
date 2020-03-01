8eec44cf84872ffc499b20901c0c75b71a14cd63
lavdimimeri <45203738+lavdimimeri@users.noreply.github.com>
2020-03-01 18:48:02 +0100

    initialization of activities
    Activities like DeliveryDetailActivity,
    ProfileActivity and DisplayOrderActivity have been added to the Project.

---
+++ shoppingapp/app/src/main/java/com/example/shoppingapplication/ProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.shoppingapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {  //Or USER SETTINGS ACTIVITY
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
