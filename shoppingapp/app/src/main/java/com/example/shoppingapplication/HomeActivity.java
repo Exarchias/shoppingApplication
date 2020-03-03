9de59292233b92a56c8a8db201e0d1a4714d9781
Abdul Barahow <barahow1994@gmail.com>
2020-03-03 14:38:41 +0100

    OrderActivity
    It isnt done yet needs to be connected to the database.

--- shoppingapp/app/src/main/java/com/example/shoppingapplication/HomeActivity.java
+++ shoppingapp/app/src/main/java/com/example/shoppingapplication/HomeActivity.java
@@ -2,16 +2,29 @@ package com.example.shoppingapplication;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     User user;
+    private ListView listItems, listItemsTwo;
+    private ImageView toogleBar, viewCart;
+    ScrollView scrollView;
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +35,50 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
             Intent i = new Intent(this, MainActivity.class);
             startActivity(i);
         } else {
+            setContentView(R.layout.activity_home);
+
+
+
             setActivityLisener();
+            getAllViews();
+            viewOnClickListeners();
         }
         // when you log in you should end up in this activity-abdul
     }
 
 
+
+
+    public void viewOnClickListeners() {
+
+        viewCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), OrdersActivity.class);
+                startActivity(intent);
+            }
+        });
+        toogleBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mDrawerLayout.openDrawer(Gravity.LEFT);
+            }
+        });
+    }
+
+
+
+
+
+
+    public void getAllViews() {
+        scrollView = (ScrollView) findViewById(R.id.scrollView);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        listItems = (ListView) findViewById(R.id.listview);
+        listItemsTwo = (ListView) findViewById(R.id.listview_two);
+        toogleBar = (ImageView) findViewById(R.id.toogleBar);
+        viewCart = (ImageView) findViewById(R.id.viewCart);
+    }
 
 
 
---
+++ shoppingapp/app/src/main/java/com/example/shoppingapplication/OrdersActivity.java
@@ -0,0 +1,122 @@
+package com.example.shoppingapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class OrdersActivity extends AppCompatActivity {
+    private ListView listOrders;
+
+    public TextView orderTotalPrice;
+    public ImageView removeButton;
+    public ConstraintLayout ordersActivity;
+    public int itemID[], itemNumPics[], orderID[];
+    public double orderTotal = 0.0;
+    public String userPaid[];
+    public ImageView closeButton;
+
+
+    User user;
+    DataHolder holder = new DataHolder();
+    Item item;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+
+        listOrders = (ListView) findViewById(R.id.listview);
+        ordersActivity = (ConstraintLayout) findViewById(R.id.OrdersActivity);
+        closeButton = (ImageView) findViewById(R.id.closeButton);
+        orderTotalPrice = (TextView) findViewById(R.id.orderTotalPrice);
+
+        int a = countOrderDatabase();
+        itemID = new int[a];
+        itemNumPics = new int[a];
+        orderID = new int[a];
+        userPaid = new String[a];
+
+
+        fetchOrders();
+        for(int i=0; i<itemID.length; i++) {
+            fetchItemsInfo(itemID[i],itemNumPics[i],orderID[i]);
+        }
+        orderTotalPrice.setText(""+orderTotal);
+    }
+// this method should fetch all itemsinformation
+    public void fetchItemsInfo(int itemID, int itemNumPics, int orderID)  {
+
+            item.setId(itemID);
+            item.setSizeInt(itemNumPics);
+            // need orderid
+
+            // i need to select from itemlist where itemid == itemID
+
+            // there is no itemprice
+           // orderTotal = orderTotal + (itemNumPics * item.Price);
+
+            DataHolder.itemPopulate().get(itemID);
+
+
+
+    }
+
+
+    public int countOrderDatabase() {
+        int a = 0;
+
+
+        DataHolder.itemIdExists(user.getId());
+
+        // if Item populate gets all the items in a list then this should work.
+        DataHolder.itemPopulate();
+        a++;
+
+
+        return a;
+
+    }
+
+    // this method fetches ordertetials usch as username and orderid. from database.
+
+    public void fetchOrdersDetail(String userName, int orderID, int index) {
+        int i = 0;
+
+        // i want to select from a username which matches an order id.
+        DataHolder.itemPopulate();
+
+
+        itemID[i] = item.getId();
+
+        // i dont know whether getownerrid is about orders or not.
+        itemNumPics[i] = item.getOwnersId();
+
+    }
+
+
+/// this one fetches all orders.
+    public void fetchOrders() {
+        int i = 0;
+
+        // i want to select from the shooping table by userid.
+        DataHolder.noteIdExists(user.getId());
+        DataHolder.notePopulate();
+                     // there should be an orderid
+        orderID[i] = item.getId();
+
+        userPaid[i] = user.getName();
+
+        i++;
+        for (int j = 0; j < i; j++) {
+            fetchOrdersDetail(userPaid[j], orderID[j], j);
+        }
+    }
+}
