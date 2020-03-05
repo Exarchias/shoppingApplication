816505ebe8ade8ce618bf49d670f1ec47b20fdff
lavdimimeri <45203738+lavdimimeri@users.noreply.github.com>
2020-03-06 00:18:46 +0100

    phone number functionallity added to the profileActivity

--- shoppingapp/app/src/main/java/com/example/shoppingapplication/ProfileActivity.java
+++ shoppingapp/app/src/main/java/com/example/shoppingapplication/ProfileActivity.java
@@ -13,16 +13,21 @@ public class ProfileActivity extends AppCompatActivity {  //Or USER SETTINGS ACT
     private NoteViewModel noteViewModel;
 
     EditText editText;
+    EditText editText1;
     Button button2;
     TextView txt;
+    TextView txt1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         button2=(Button)findViewById(R.id.button2);
         editText=(EditText)findViewById(R.id.fullname_editText_ProfileA);
+        editText1=(EditText)findViewById(R.id.phonenumber_editText_ProfileA);
         noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
         txt=(TextView)findViewById(R.id.textView4);
+        txt1=(TextView)findViewById(R.id.textView5);
+
 
 
 
@@ -32,11 +37,15 @@ public class ProfileActivity extends AppCompatActivity {  //Or USER SETTINGS ACT
 
     public void onClick(View view){
         User tmpUser = DataHolder.arrayAllUsers.get(2);
-        String fullname;
+        String fullname,phonenumber;
         fullname=editText.getText().toString();
+        phonenumber=editText1.getText().toString();
         tmpUser.setFullName(fullname);
+        tmpUser.setMobilePhone(phonenumber);
+
         noteViewModel.useThatUpdateUser(tmpUser);
         txt.setText(DataHolder.arrayAllUsers.get(2).getFullName());
+        txt1.setText(DataHolder.arrayAllUsers.get(2).getMobilePhone());
 
 
 
