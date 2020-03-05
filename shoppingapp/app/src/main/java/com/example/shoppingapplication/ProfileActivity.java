a7be4fe8f6a8d596e0be8fe34d8e035f35f7f239
lavdimimeri <45203738+lavdimimeri@users.noreply.github.com>
2020-03-06 00:48:36 +0100

    Address functionallity added to the profileActivity

--- shoppingapp/app/src/main/java/com/example/shoppingapplication/ProfileActivity.java
+++ shoppingapp/app/src/main/java/com/example/shoppingapplication/ProfileActivity.java
@@ -14,9 +14,11 @@ public class ProfileActivity extends AppCompatActivity {  //Or USER SETTINGS ACT
 
     EditText editText;
     EditText editText1;
+    EditText editText3;
     Button button2;
     TextView txt;
     TextView txt1;
+    TextView txt2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,10 +26,11 @@ public class ProfileActivity extends AppCompatActivity {  //Or USER SETTINGS ACT
         button2=(Button)findViewById(R.id.button2);
         editText=(EditText)findViewById(R.id.fullname_editText_ProfileA);
         editText1=(EditText)findViewById(R.id.phonenumber_editText_ProfileA);
+        editText3=(EditText)findViewById(R.id.address_editText_ProfileA);
         noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
         txt=(TextView)findViewById(R.id.textView4);
         txt1=(TextView)findViewById(R.id.textView5);
-
+        txt2=(TextView)findViewById(R.id.textView6);
 
 
 
@@ -37,15 +40,18 @@ public class ProfileActivity extends AppCompatActivity {  //Or USER SETTINGS ACT
 
     public void onClick(View view){
         User tmpUser = DataHolder.arrayAllUsers.get(2);
-        String fullname,phonenumber;
+        String fullname,phonenumber,address;
         fullname=editText.getText().toString();
         phonenumber=editText1.getText().toString();
+        address = editText3.getText().toString();
         tmpUser.setFullName(fullname);
         tmpUser.setMobilePhone(phonenumber);
+        tmpUser.setAddress(address);
 
         noteViewModel.useThatUpdateUser(tmpUser);
         txt.setText(DataHolder.arrayAllUsers.get(2).getFullName());
         txt1.setText(DataHolder.arrayAllUsers.get(2).getMobilePhone());
+        txt2.setText(DataHolder.arrayAllUsers.get(2).getAddress());
 
 
 
