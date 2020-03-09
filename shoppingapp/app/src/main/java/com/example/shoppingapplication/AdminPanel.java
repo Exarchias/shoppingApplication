783ca3f1de2d5d276678ce85e4827feb7288a45a
lavdimimeri <45203738+lavdimimeri@users.noreply.github.com>
2020-03-09 23:17:06 +0100

    necessary code added to the AdminPanelActivity

--- shoppingapp/app/src/main/java/com/example/shoppingapplication/AdminPanel.java
+++ shoppingapp/app/src/main/java/com/example/shoppingapplication/AdminPanel.java
@@ -1,14 +1,19 @@
 package com.example.shoppingapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
 
 public class AdminPanel extends AppCompatActivity {
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_panel);
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
     }
+
+    
 }
