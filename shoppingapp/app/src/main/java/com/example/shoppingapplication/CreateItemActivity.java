f94b650dad994a07327d82fe0c4ee4639085ced4
lavdimimeri <45203738+lavdimimeri@users.noreply.github.com>
2020-03-17 00:31:26 +0100

    Basic layout for the createItem activity added

--- shoppingapp/app/src/main/java/com/example/shoppingapplication/CreateItemActivity.java
+++ shoppingapp/app/src/main/java/com/example/shoppingapplication/CreateItemActivity.java
@@ -3,12 +3,23 @@ package com.example.shoppingapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class CreateItemActivity extends AppCompatActivity {
+    EditText title_editText_CreateItem;
+    EditText description_editText_CreateItem;
+    EditText price_editText_CreateItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_item);
+    }
+
+
+
+    public void onClickUpdate(View view){
+
     }
 }
