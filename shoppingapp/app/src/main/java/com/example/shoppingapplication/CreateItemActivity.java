a9a0981fafd6cc040bac7f08b16e93a9e9875feb
AkliluBerhe <eritrea12>
2020-03-17 21:55:49 +0100

    update

--- shoppingapp/app/src/main/java/com/example/shoppingapplication/CreateItemActivity.java
+++ shoppingapp/app/src/main/java/com/example/shoppingapplication/CreateItemActivity.java
@@ -1,14 +1,61 @@
 package com.example.shoppingapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CreateItemActivity extends AppCompatActivity {
+    private NoteViewModel noteViewModel;
+    EditText title_editText_CreateItem;
+    EditText description_editText_CreateItem;
+    EditText price_editText_CreateItem;
+    Button btn_createItem_CreateItemActivity;
+    TextView textViewDescription;
+    TextView textViewTitle;
+    TextView textViewPrice;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_item);
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+        title_editText_CreateItem=(EditText)findViewById(R.id.title_editText_CreateItem);
+        description_editText_CreateItem=(EditText)findViewById(R.id.description_editText_CreateItem);
+        price_editText_CreateItem=(EditText)findViewById(R.id.price_editText_CreateItem);
+        btn_createItem_CreateItemActivity=(Button)findViewById(R.id.btn_createItem_CreateItemActivity);
+        textViewDescription=(TextView)findViewById(R.id.textViewDescription);
+        textViewPrice=(TextView)findViewById(R.id.textViewPrice);
+        textViewTitle=(TextView)findViewById(R.id.textViewTitle);
+
+
+
+    }
+
+
+
+    public void onClickUpdate(View view){
+      Item tmpItem = DataHolder.arrayAllItems.get(2);
+      String title,description,price;
+
+      title=title_editText_CreateItem.getText().toString();
+      price=price_editText_CreateItem.getText().toString();
+      description=description_editText_CreateItem.getText().toString();
+      tmpItem.setTitle(title);
+     // tmpItem.setPrice(price);
+      tmpItem.setDescription(description);
+
+      noteViewModel.useThatCreateItem(tmpItem);
+      textViewTitle.setText(DataHolder.arrayAllItems.get(2).getTitle());
+     // textViewPrice.setText(DataHolder.arrayAllItems.get(2).getPrice());
+      textViewDescription.setText(DataHolder.arrayAllItems.get(2).getDescription());
+
+
+
     }
 }
--- shoppingapp/app/src/main/java/com/example/shoppingapplication/ForgetPasswordActivity.java
+++ shoppingapp/app/src/main/java/com/example/shoppingapplication/ForgetPasswordActivity.java
@@ -1,8 +1,11 @@
 package com.example.shoppingapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,53 +14,157 @@ import android.widget.Toast;
 
 import com.example.shoppingapplication.gmailSender.GmailSender;
 
+<<<<<<< HEAD
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+=======
+import java.security.SecureRandom;
+>>>>>>> 3e6b797a90552373f6402020c2215d1e74d8edac
+
 public class ForgetPasswordActivity extends AppCompatActivity {
 
     private MailSender mailSender;
 
     private EditText emailEditText;
     private Button sendToEmail;
+<<<<<<< HEAD
+    GmailSender gmailSender;
+    // new ones
+    public String jobNo;
+    public String teamNo;
+    private static final String username = "berheaklilu1@gmail.com";
+    private static final String password = "Eritrea17";
+    private static final String emailid = "mail2@outlook.com";
+    private static final String subject = "Photo";
+    private static final String message = "Hello";
+    private Multipart multipart = new MimeMultipart();
+    private MimeBodyPart messageBodyPart = new MimeBodyPart();
+    public File mediaFile;
+=======
+    private NoteViewModel noteViewModel;
+>>>>>>> 3e6b797a90552373f6402020c2215d1e74d8edac
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_password);
-
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
 
         emailEditText = findViewById(R.id.email_edit_text);
         sendToEmail = findViewById(R.id.send_to_email);
+
+        //new ones
+
+        Intent intent = getIntent();
+        jobNo = intent.getStringExtra("Job_No");
+        teamNo = intent.getStringExtra("Team_No");
+        //sendMail(emailid, subject, message);
 
 
         sendToEmail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                // change an activity when admin logged in/ send to Admin page
-                //Send mail.
+                //sendMail(emailid, subject, message);
 
-                mailSender = new MailSender();  // make sure the user with phon existes.
                 try {
-                    //boolean check = mailSender.sendEmail();
-                    //if (check){
-                        Toast.makeText(ForgetPasswordActivity.this, "Succssess", Toast.LENGTH_SHORT).show();
-                    //}
+
+                    String email = "berheaklilu1@gmail.com";  // temp gmail account to send mails from
+                    String pass = "Eritrea17";
+                    GmailSender gmailSender = new GmailSender(email,pass);
+                    gmailSender.sendMail("test", "Hello gmail","berheaklilu1@gmail.com", "karl.i.lundh@gmail.com");
                 } catch (Exception e) {
-                    Toast.makeText(ForgetPasswordActivity.this, "Fsil", Toast.LENGTH_SHORT).show();
                     e.printStackTrace();
                 }
-
-                Intent intent = new Intent(ForgetPasswordActivity.this, LoginActivity.class);
-                startActivity(intent);
             }
+
         });
 
     }
 
-    public void verifyEmail(){
+    private void sendMail(String email, String subject, String messageBody) {
+        Session session = createSessionObject();
 
-        if (emailEditText.getText().toString().isEmpty()){
-            Toast.makeText(this, "please fill you valid email", Toast.LENGTH_SHORT).show();
+        try {
+            Message message = createMessage(email, subject, messageBody, session);
+            new SendMailTask().execute(message);
+        } catch (AddressException e) {
+            e.printStackTrace();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
         }
+    }
+
+    private Session createSessionObject() {
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com");//"smtp.mailtrap.io";
+        properties.put("mail.smtp.port", "587");
+
+        return Session.getInstance(properties, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+    }
+
+<<<<<<< HEAD
+    private Message createMessage(String email, String subject, String messageBody, Session session) throws
+            MessagingException, UnsupportedEncodingException {
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress("berheaklilu1@gmail.com", "Naveed Qureshi"));
+        message.addRecipient(Message.RecipientType.TO, new InternetAddress(email, email));
+        message.setSubject(subject);
+        message.setText(messageBody);
+        return message;
+    }
+
+    public class SendMailTask extends AsyncTask<Message, Void, Void> {
+        private ProgressDialog progressDialog;
+=======
+
+    }
+
+
+    public void verifyEmail(){
+>>>>>>> 3e6b797a90552373f6402020c2215d1e74d8edac
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = ProgressDialog.show(ForgetPasswordActivity.this, "Please wait", "Sending mail", true, false);
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            progressDialog.dismiss();
+        }
+        protected Void doInBackground(javax.mail.Message... messages) {
+            try {
+                Transport.send(messages[0]);
+            } catch (MessagingException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
     }
 }
