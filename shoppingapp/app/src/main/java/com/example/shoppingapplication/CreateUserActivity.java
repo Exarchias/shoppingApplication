e92ee89db1c7457f1945f5ac2a317a91998c751c
lavdimimeri <45203738+lavdimimeri@users.noreply.github.com>
2020-03-18 22:40:33 +0100

    Full functionality added to the CreateUser Activity

--- shoppingapp/app/src/main/java/com/example/shoppingapplication/CreateUserActivity.java
+++ shoppingapp/app/src/main/java/com/example/shoppingapplication/CreateUserActivity.java
@@ -3,6 +3,7 @@ package com.example.shoppingapplication;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,14 +15,11 @@ import android.widget.Toast;
 public class CreateUserActivity extends AppCompatActivity {
     private NoteViewModel noteViewModel;
     EditText editName;
-    EditText fullname;
+    EditText phoneNumberEditText;
     EditText address;
     EditText passwordEditText;
-    EditText userIdEditText;
-    TextView testing1;
-    TextView testing2;
-    TextView testing3;
-    TextView testing4;
+    EditText emailAddressEditText;
+
     Button createUser;
     CheckBox checkBox;
 
@@ -32,26 +30,27 @@ public class CreateUserActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_user);
         noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
         editName=(EditText)findViewById(R.id.name_editText_CreateUser);
-        fullname=(EditText)findViewById(R.id.fullname_editText_CreateUser);
+        phoneNumberEditText=(EditText)findViewById(R.id.phoneNumber_editText_CreateUser);
+        emailAddressEditText=(EditText)findViewById(R.id.emailAddress_editText_CreateUser);
         passwordEditText =(EditText)findViewById(R.id.password_editText_CreateUser);
         address=(EditText)findViewById(R.id.address_editText_CreateUser);
-        testing1=(TextView)findViewById(R.id.textViewFullNameCreateUserActivity);
-        testing2=(TextView)findViewById(R.id.textViewNameCreateUserActivity);
-        testing3=(TextView)findViewById(R.id.textViewPhoneCreateUserActivity);
-        testing4=(TextView)findViewById(R.id.textViewAddressCreateUserActivity);
         createUser=(Button)findViewById(R.id.btn_createUser_CreateUserActivity);
         checkBox=(CheckBox)findViewById(R.id.checkBox);
-        userIdEditText =(EditText)findViewById(R.id.UserID_editText_CreateUser);
+
     }
 
     public void onClick(View view){
 
         final User tmp = new User(5000,"",false,"12345");
-        String name,password,userId;
+        String name,password,phoneNumber,emailAddress;
         Boolean istheUserAdmin;
-      //  userId=userIdEditText.getText().toString();
+        phoneNumber=phoneNumberEditText.getText().toString();
+        tmp.setMobilePhone(phoneNumber);
+
+        emailAddress=emailAddressEditText.getText().toString();
+        tmp.setEmail(emailAddress);
         password = passwordEditText.getText().toString();
-      //  tmp.setId(Integer.parseInt(userId));
+
         tmp.setPassword(password);
         name = editName.getText().toString();
         tmp.setName(name);
@@ -74,12 +73,17 @@ public class CreateUserActivity extends AppCompatActivity {
 
          //STUFF USED FOR TESTING
 
-      // final  int last = DataHolder.arrayAllUsers.size()-1;
-      //  testing2.setText(DataHolder.arrayAllUsers.get(last).getName());
+
         Toast.makeText(CreateUserActivity.this,"User is admin: "+tmp.isAdmin(),Toast.LENGTH_SHORT).show();
-      //  testing3.setText(DataHolder.arrayAllUsers.get(last).getPassword());
 
 
+        Intent intent = new Intent(CreateUserActivity.this, AdminPanel.class);
+        CreateUserActivity.this.startActivity(intent);
 
+    }
+
+    public void closeBtn(View view){
+        Intent intent = new Intent(CreateUserActivity.this, HomeActivity.class);
+        CreateUserActivity.this.startActivity(intent);
     }
 }
