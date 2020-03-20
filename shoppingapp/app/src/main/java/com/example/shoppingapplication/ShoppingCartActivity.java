b4292848731359bc8665914081b3640aa9e0ca88
Robert Kristian Alm <exarchiasghost@gmail.com>
2020-03-20 04:08:54 +0100

    shopping cart displayis the selected items.
    The items that the user selects to send to the shopping card now they are displayed to the shopping cart.
    Now I just have to add somefunctionality that sends to the checkout.

--- shoppingapp/app/src/main/java/com/example/shoppingapplication/ShoppingCartActivity.java
+++ shoppingapp/app/src/main/java/com/example/shoppingapplication/ShoppingCartActivity.java
@@ -3,12 +3,27 @@ package com.example.shoppingapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ShoppingCartActivity extends AppCompatActivity {
+    ListView shoppingCartListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_cart);
+        shoppingCartListView = (ListView)findViewById(R.id.shoppingCartListView);
+        populateCartListView();
+    }
+
+    void populateCartListView(){
+        //the items that are collected to the shopping cart
+        ArrayList<Item> arrayOfItems = DataHolder.arrayCartItems;
+        // Create the adapter to convert the array to views
+        RItemAdapter adapter = new RItemAdapter(ShoppingCartActivity.this, arrayOfItems);
+        // Attach the adapter to a ListView
+        shoppingCartListView.setAdapter(adapter);
     }
 }
