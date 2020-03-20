91305ff0a19d797f5f654ab75553c75e87c1529a
Robert Kristian Alm <exarchiasghost@gmail.com>
2020-03-20 04:41:02 +0100

    small break before we call it a day.
    I added the button and i added at least the funtionality of movieng to another activity.
    Now the only thing that remains is to add some nice functionality and we can call it a day.
    But for now I will need a break.

--- shoppingapp/app/src/main/java/com/example/shoppingapplication/ShoppingCartActivity.java
+++ shoppingapp/app/src/main/java/com/example/shoppingapplication/ShoppingCartActivity.java
@@ -2,20 +2,34 @@ package com.example.shoppingapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class ShoppingCartActivity extends AppCompatActivity {
     ListView shoppingCartListView;
+    Button toCheckOutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_cart);
         shoppingCartListView = (ListView)findViewById(R.id.shoppingCartListView);
+        toCheckOutBtn = (Button)findViewById(R.id.toCheckOutBtn);
         populateCartListView();
+
+        toCheckOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //HomeActivity here needs to be replaced with the activity where the checkout will take place.
+                Intent intent = new Intent(ShoppingCartActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     void populateCartListView(){
