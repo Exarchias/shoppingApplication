3b0ae363e2ccbc9c8bb5a24deececa6f6ae76b49
Robert Kristian Alm <exarchiasghost@gmail.com>
2020-03-23 05:24:48 +0100

    Making me a customer
    I added a hardcoded account for myself.
    Testing purposes

--- shoppingapp/app/src/main/java/com/example/shoppingapplication/DataHolder.java
+++ shoppingapp/app/src/main/java/com/example/shoppingapplication/DataHolder.java
@@ -10,7 +10,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 public class DataHolder {
-    static int nextUserId = 5; //it is used for autoincrement of the id.
+    static int nextUserId = 6; //it is used for autoincrement of the id.
     static int nextItemId = 7; //it is used for autoincrement of the id.
     static int nextNoteId = 7; //it is used for autoincrement of the id.
 
@@ -54,6 +54,7 @@ public class DataHolder {
         tempAr.add(new User(2, "Alice", false, RTools.encrypted("12345"), "alice@test.clom", "2345678901"));
         tempAr.add(new User(3, "Bob", false, RTools.encrypted("12345"), "Bob@test.clom", "3456789012"));
         tempAr.add(new User(4, "Aki", false, RTools.encrypted("1000"), "aki@test.clom", "0764129365"));
+        tempAr.add(new User(5, "Robert", true, RTools.encrypted("1"), "RobertKristianAlm@gmail.com", "0760882382"));
         return tempAr;
     }
 
